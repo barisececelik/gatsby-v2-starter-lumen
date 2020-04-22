75656722047a0a6fe62ee50b5818c54e9b416ce6
barisececelik <barisececelik@gmail.com>
2020-04-22 10:19:37 +0300

    3b

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -6,11 +6,8 @@ class Links extends React.Component {
   render() {
     const author = this.props.data
     const links = {
-      telegram: author.telegram,
       twitter: author.twitter,
       github: author.github,
-      vk: author.vk,
-      rss: author.rss,
       email: author.email,
     }
 
@@ -28,15 +25,6 @@ class Links extends React.Component {
           </li>
           <li className="links__list-item">
             <a
-              href={`https://www.linkedin.com/barisececelik`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="icon-linkedin" />
-            </a>
-          </li>
-          <li className="links__list-item">
-            <a
               href={`https://www.github.com/${links.github}`}
               target="_blank"
               rel="noopener noreferrer"
@@ -44,6 +32,28 @@ class Links extends React.Component {
               <i className="icon-github" />
             </a>
           </li>
+          <li className="links__list-item">
+            <a
+              href={`https://www.vk.com/${links.vk}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="icon-vkontakte" />
+            </a>
+          </li>
+        </ul>
+        <ul className="links__list">
+          <li className="links__list-item">
+            <a href={`mailto:${links.email}`}>
+              <i className="icon-mail" />
+            </a>
+          </li>
+          <li className="links__list-item">
+            <a href={`telegram:${links.telegram}`}>
+              <i className="icon-paper-plane" />
+            </a>
+          </li>
+        </ul>
         <ul className="links__list">
           <li className="links__list-item">
             <a href={links.rss}>
